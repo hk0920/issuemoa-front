428f8e1d6fcd29fa5effd4919d015ff9df813b92
hk0920 <gmlrb920@naver.com>
2024-01-03 23:17:30 +0900

    [modify] grade 컴포넌트 > icon 이미지 사이즈 조절, 정렬 수정

--- src/components/more/grade.tsx
+++ src/components/more/grade.tsx
@@ -1,38 +1,28 @@
-import { Container, Image } from 'react-bootstrap';
-import {
-  grade_br,
-  grade_ch,
-  grade_dia,
-  grade_gd,
-  grade_gm,
-  grade_ir,
-  grade_m,
-  grade_plt,
-  grade_sl,
-} from '../../images';
+import { Container, Image } from "react-bootstrap";
+import { grade_br, grade_ch, grade_dia, grade_gd, grade_gm, grade_ir, grade_m, grade_plt, grade_sl } from "../../images";
 
 const Grade = () => {
   // 이미지와 설명을 나타내는 데이터 배열
   const gradeData = [
-    { image: grade_ch, grade: 'Grade CH', description:'Grade CH 등급은 1000개 이상 일 경우 승급' },
-    { image: grade_gm, grade: 'Grade GM', description:'Grade GM 등급은 700개 이상 일 경우 승급' },
-    { image: grade_m, grade: 'Grade M', description:'Grade M 등급은 500개 이상 일 경우 승급' },
-    { image: grade_dia, grade: 'Grade DIA', description:'Grade DIA 등급은 300개 이상 일 경우 승급' },
-    { image: grade_plt, grade: 'Grade PLT', description:'Grade PLT 등급은 200개 이상 일 경우 승급' },
-    { image: grade_gd, grade: 'Grade GD', description:'Grade GD 등급은 100개 이상 일 경우 승급' },
-    { image: grade_sl, grade: 'Grade SL', description:'Grade SG 등급은 50개 이상 일 경우 승급' },
-    { image: grade_br, grade: 'Grade BR', description:'Grade BR 등급은 20개 이상 일 경우 승급' },
-    { image: grade_ir, grade: 'Grade IR', description:'Grade IR 등급은 10개 이상 일 경우 승급' },
+    { image: grade_ch, grade: "Grade CH", description: "Grade CH 등급은 1000개 이상 일 경우 승급" },
+    { image: grade_gm, grade: "Grade GM", description: "Grade GM 등급은 700개 이상 일 경우 승급" },
+    { image: grade_m, grade: "Grade M", description: "Grade M 등급은 500개 이상 일 경우 승급" },
+    { image: grade_dia, grade: "Grade DIA", description: "Grade DIA 등급은 300개 이상 일 경우 승급" },
+    { image: grade_plt, grade: "Grade PLT", description: "Grade PLT 등급은 200개 이상 일 경우 승급" },
+    { image: grade_gd, grade: "Grade GD", description: "Grade GD 등급은 100개 이상 일 경우 승급" },
+    { image: grade_sl, grade: "Grade SL", description: "Grade SG 등급은 50개 이상 일 경우 승급" },
+    { image: grade_br, grade: "Grade BR", description: "Grade BR 등급은 20개 이상 일 경우 승급" },
+    { image: grade_ir, grade: "Grade IR", description: "Grade IR 등급은 10개 이상 일 경우 승급" },
   ];
 
   return (
     <Container className="page__sub">
-      {gradeData.length > 0?
+      {gradeData.length > 0 ? (
         <ul className="list__grade">
           {gradeData.map((item, index) => (
             <li key={index} className="list-item">
               <div className="box__icon">
-                <Image src={item.image} alt={`Grade ${index + 1}`}  />
+                <Image src={item.image} alt={`Grade ${index + 1}`} className="image" />
               </div>
               <div className="box__info">
                 <p className="text__title">{item.grade}</p>
@@ -41,7 +31,7 @@ const Grade = () => {
             </li>
           ))}
         </ul>
-      : null}
+      ) : null}
     </Container>
   );
 };
